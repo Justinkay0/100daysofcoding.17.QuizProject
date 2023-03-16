87e1658596b1d7d2dffde97fa3e66b51ffe9e53f
Justinkay0 <14783039+Justinkay0@users.noreply.github.com>
2023-03-16 16:34:29 +0800

    Creating Methods

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User('619', 'Rey Mysterio')
 user_2 = User('888', 'Triple H')
 
+user_1.follow(user_2)
+print(user_1.following)
+print(user_1.followers)
